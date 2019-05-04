c3014063d8e8f55d4831b727753875e929edacf0
Tthienphuc147 <dthienphuc147@gmail.com>
2019-05-04 19:24:32 +0700

    layout

---
+++ resources/views/block/header.blade.php
@@ -0,0 +1,44 @@
+<div id="preloder">
+    <div class="loader"></div>
+</div>
+
+<!-- header section -->
+<header class="header-section">
+    <div class="container">
+        <!-- logo -->
+        <a href="/" class="site-logo"><img src="public/img/logo.png" alt=""></a>
+        <div class="nav-switch">
+            <i class="fa fa-bars"></i>
+        </div>
+        <div class="header-info">
+            <div class="hf-item">
+                <i class="fa fa-clock-o"></i>
+                <p><span>Working time:</span>Monday - Friday: 08 AM - 06 PM</p>
+            </div>
+            <div class="hf-item">
+                <i class="fa fa-map-marker"></i>
+                <p><span>Find us:</span>40 Baria Street 133/2, New York City, US</p>
+            </div>
+        </div>
+    </div>
+</header>
+<!-- header section end-->
+
+
+<!-- Header section  -->
+<nav class="nav-section">
+    <div class="container">
+        <div class="nav-right">
+            <a href=""><i class="fa fa-search"></i></a>
+            <a href=""><i class="fa fa-shopping-cart"></i></a>
+        </div>
+        <ul class="main-menu">
+            <li class="active"><a href="">Home</a></li>
+            <li><a href="">About Us</a></li>
+            <li><a href="">Event</a></li>
+            <li><a href="">COURSES</a></li>
+            <li><a href="">blog</a></li>
+            <li><a href="">Contact</a></li>
+        </ul>
+    </div>
+</nav>
---
+++ resources/views/master.blade.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>@yield('title')</title>
+
+    <link href="public/img/favicon.ico" rel="shortcut icon"/>
+
+    <link rel="stylesheet" href="{{ asset('public/css/bootstrap.min.css') }} ">
+    <link rel="stylesheet" href="{{ asset('public/css/font-awesome.min.css') }} ">
+    <link rel="stylesheet" href="{{ asset('public/css/themify-icons.css') }} ">
+    <link rel="stylesheet" href="{{ asset('public/css/magnific-popup.css') }} ">
+    <link rel="stylesheet" href="{{ asset('public/css/animate.css') }} ">
+    <link rel="stylesheet" href="{{ asset('public/css/owl.carousel.css') }} ">
+    <link rel="stylesheet" href="{{ asset('public/css/style.css') }} ">
+</head>
+<body>
+@include('block.header')
+@include('block.slider')
+
+@yield('content')
+
+@include('block.footer')
+
+<script type='text/javascript' src='{{ asset('public/js/jquery-3.2.1.min.js') }}'></script>
+<script type='text/javascript' src='{{ asset('public/js/owl.carousel.min.js') }}'></script>
+<script type='text/javascript' src='{{ asset('public/js/jquery.countdown.js') }}'></script>
+<script type='text/javascript' src='{{ asset('public/js/masonry.pkgd.min.js') }}'></script>
+<script type='text/javascript' src='{{ asset('public/js/magnific-popup.min.js') }}'></script>
+<script type='text/javascript' src='{{ asset('public/js/main.js') }}'></script>
+
+
+
+
+</body>
+</html>
