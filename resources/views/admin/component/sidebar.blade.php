5c02143186e530d5fd9ff19f9bd83080f60f9c50
nghiadh2016 <47706371+nghiadh2016@users.noreply.github.com>
2020-03-14 00:37:46 +0700

    bo sidebar dư
    bo sidebar dư

--- resources/views/admin/component/sidebar.blade.php
+++ resources/views/admin/component/sidebar.blade.php
@@ -56,46 +56,7 @@
             </ul>
           </li>
           @endif
-          <li class="sub-menu">
-            <a href="javascript:;">
-              <i class="fa fa-book" aria-hidden="true"></i>
-              <span>Quản lý bài học</span>
-              </a>
-            <ul class="sub">
-              <li><a href="">Danh sách bài học</a></li>
-              <li><a href="">Thêm bài học</a></li>
-            </ul>
-          </li>
-          <li class="sub-menu">
-            <a href="javascript:;">
-              <i class="fa fa-book" aria-hidden="true"></i>
-              <span>Quản lý loại bài học</span>
-              </a>
-            <ul class="sub">
-              <li><a href="/loaibaihoc">Danh sách loại bài học</a></li>
-              <li><a href="/loaibaihoc/create">Thêm loại bài học</a></li>
-            </ul>
-          </li>
-          <li class="sub-menu">
-            <a href="javascript:;">
-              <i class="fa fa-book" aria-hidden="true"></i>
-              <span>Quản lý loại trắc nghiệm</span>
-              </a>
-            <ul class="sub">
-              <li><a href="">Danh sách loại trắc nghiệm</a></li>
-              <li><a href="">Thêm loại trắc nghiệm</a></li>
-            </ul>
-          </li>
-          <li class="sub-menu">
-            <a href="javascript:;">
-              <i class="fa fa-book" aria-hidden="true"></i>
-              <span>Quản lý môn học</span>
-              </a>
-            <ul class="sub">
-              <li><a href="">Danh sách môn học</a></li>
-              <li><a href="">Thêm môn học</a></li>
-            </ul>
-          </li>
+          
           <li class="sub-menu">
             <a href="javascript:;">
               <i class="fa fa-book" aria-hidden="true"></i>
@@ -106,26 +67,7 @@
               <li><a href="/mucdo/create">Thêm mức độ bài học</a></li>
             </ul>
           </li>
-          <li class="sub-menu">
-            <a href="javascript:;">
-              <i class="fa fa-book" aria-hidden="true"></i>
-              <span>Quản lý loại đáp án</span>
-              </a>
-            <ul class="sub">
-              <li><a href="">Danh sách đáp án</a></li>
-              <li><a href="">Quản lý đáp án</a></li>
-            </ul>
-          </li>
-          <li class="sub-menu">
-            <a href="javascript:;">
-              <i class="fa fa-book" aria-hidden="true"></i>
-              <span>Quản lý học viên</span>
-              </a>
-            <ul class="sub">
-              <li><a href="">Danh sách học viên</a></li>
-              <li><a href="">Quản lý</a></li>
-            </ul>
-          </li>
+         
           @if(request()->session()->get('role')==2)
           <li class="sub-menu">
             <a href="javascript:;">
