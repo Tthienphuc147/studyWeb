94d3e5e8e74724a63a51523519b738cbf47bc07d
Phan Minh Hai <pmhai1998@gmail.com>
2020-03-13 19:23:37 +0700

    update monhoc-lophoc

--- app/Http/Controllers/baihocController.php
+++ app/Http/Controllers/baihocController.php
@@ -6,27 +6,39 @@ use App\lophoc;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Http\Request;
 use Carbon\Carbon;
+use Session;
 class baihocController extends Controller
 {
 
 
     public function show($id,$id1,Request $request)
     {
+        
         if( $request->session()->has('id'))
         {
+            
+            $check=DB::table('chitietlophoc_monhoc')->where('id_lophoc',$id)->where('id_monhoc',$id1)
+            ->join('chitietlop_user','chitietlophoc_monhoc.id','chitietlop_user.id_chitietlophoc_monhoc')
+            ->where('chitietlop_user.id_user',session()->get('id'))
+            ->where('chitietlop_user.status',1)->get();
+
+            if (count($check)==0) {
+                Session::flash('error', 'Bạn chưa được phép vào lớp học này do chưa đăng ký hoặc chưa được chấp nhận');
+                return redirect("/lophoc/$id");
+            }
             $data1=DB::table('chitietlophoc_monhoc')->where('id_lophoc',$id)->where('id_monhoc',$id1)->first();
 
-        $databaihoc = DB::table('loaibaihoc')
-        ->join('baihoc','baihoc.id_loaibai','=','loaibaihoc.id')
-        ->where('id_chitietlophoc_monhoc',$data1->id)
-        ->where('baihoc.id_loaibai',2)
-        ->get();
-        $datathi=DB::table('loaibaihoc')
-        ->join('baihoc','baihoc.id_loaibai','=','loaibaihoc.id')
-        ->where('id_chitietlophoc_monhoc',$data1->id)
-        ->where('baihoc.id_loaibai',1)
-        ->get();
-        return view('page.baihoc')->with('databaihoc',$databaihoc)->with('datathi',$datathi);
+            $databaihoc = DB::table('loaibaihoc')
+            ->join('baihoc','baihoc.id_loaibai','=','loaibaihoc.id')
+            ->where('id_chitietlophoc_monhoc',$data1->id)
+            ->where('baihoc.id_loaibai',2)
+            ->get();
+            $datathi=DB::table('loaibaihoc')
+            ->join('baihoc','baihoc.id_loaibai','=','loaibaihoc.id')
+            ->where('id_chitietlophoc_monhoc',$data1->id)
+            ->where('baihoc.id_loaibai',1)
+            ->get();
+            return view('page.baihoc')->with('databaihoc',$databaihoc)->with('datathi',$datathi);
         }
         return Redirect('/loginview');
 
--- resources/views/page/lophoc.blade.php
+++ resources/views/page/lophoc.blade.php
@@ -5,12 +5,22 @@ Lớp học
 @section('content')
 <div class="site-breadcrumb">
 		<div class="container">
+
 			<a href="#"><i class="fa fa-home"></i> Home</a> <i class="fa fa-angle-right"></i>
 			<span>Môn học</span>
 		</div>
 	</div>
 <section class="full-courses-section spad pt-0">
 		<div class="container">
+		@if ( Session::has('error') )
+        <div class="alert alert-danger alert-dismissible" role="alert">
+            <strong>{{ Session::get('error') }}</strong>
+            <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                <span aria-hidden="true">&times;</span>
+                <span class="sr-only">Close</span>
+            </button>
+        </div>
+    	@endif     
 			<div class="row">
                 <!-- course item -->
                 @foreach ($datamonhoc as $item)
